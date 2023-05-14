23afc04f5d23b8e6cf8c29a11402a94c45313c33
Raymond1415926 <yunruih2@illinois.edu>
2023-05-14 18:54:46 -0500

    dialog box

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const dialog = document.querySelector('#login');
 const trigger = document.querySelector('#careersGPT-link');
+
 trigger.addEventListener('click', () => {
     dialog.showModal();
+})
+
+document.addEventListener('click', (e) => {
+    if (e.target === dialog) {
+        dialog.close();
+    }
 })
