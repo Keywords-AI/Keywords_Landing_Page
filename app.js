6548fa309dc022ec306584876d6ffd8a6740a8af
Zihuan Mao <zihuanm2@illinois.edu>
2023-05-15 23:27:18 -0500

    tbd:oauth, db

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const dialog = document.querySelector('#login');
 const trigger = document.querySelector('#careersGPT-link');
+const dialog_sign = document.querySelector('#signup');
+const trigger_sign = document.querySelector('#signup-link');
+const trigger_login = document.querySelector('#login-link');
 
 trigger.addEventListener('click', () => {
     dialog.showModal();
 })
 
-document.addEventListener('click', (e) => {
-    if (e.target === dialog) {
-        dialog.close();
-    }
+trigger_sign.addEventListener('click', () => {
+    dialog.close();
+    dialog_sign.showModal();
+})
+
+trigger_login.addEventListener('click', () => {
+    dialog_sign.close();
+    dialog.showModal();
+    console.log("Dialog closed");
 })
